533c0d53f82a75858ed88601a99ed4655b98fc66
owner <alswns6327@naver.com>
2023-11-21 17:23:45 +0900

    composition api

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,23 +1,48 @@
 <template>
-    <div>
-        x : <input type="text" v-model.number="x"/><br/>
-        y : <input type="text" v-model.number="y"/>
-        <button @click="clickBtn">click</button>{{result}}
+    <div><button @click="testBtn">test</button>{{result}}<br/><br/>
+        x : <input type="text" v-model.number="data.x"/><br/>
+        y : <input type="text" v-model.number="data.y"/>
+        <button @click="clickBtn">click</button>{{result}}<br/><br/>
+        x : <input type="text" v-model.number="data2.x"/><br/>
+        y : <input type="text" v-model.number="data2.y"/>
+        <button @click="clickBtn2">click</button>{{data2.result}}<br/><br/>
+
+        x : <input type="text" v-model.number="data3"/><br/>
+        y : <input type="text" v-model.number="data3"/>{{ result3 }}
+        <br/><br/>
+        data4.x : <input type="text" v-model.number="data4.x"/>
+        data4.result : <input type="text" v-model.number="data4.result"/>
     </div>
 </template>
 
 <script>
-import { computed } from '@vue/reactivity';
-import { ref } from 'vue';
+// import { computed } from '@vue/reactivity';
+import { computed, reactive, ref, watch } from 'vue';
 
     export default {
         name : "Calc",
         setup(){
-            const x = ref(10);
-            const y = ref(20);
-            const result = computed(()=>x.value+y.value);
+            const data = ref({x : 10, y: 20});
+            const result = computed(()=>data.value.x+data.value.y);
             const clickBtn = ()=>{console.log(result.value);}
-            return {x, y,  result, clickBtn}
+
+            const data2 = reactive({x : 30, y : 80, result : 30 + 70});
+            const clickBtn2 = ()=>{data2.result = data2.x + data2.y; console.log(data2.result);}
+
+            // const data3 = reactive({x : 1000, y:3000});
+            const data3 = ref(100);
+            const result3 = ref(data3.value * 2);
+            watch(data3, (c, o)=> {
+                console.log(`${c} ${c} before ${o} ${o}`);
+                result3.value = c * 2;
+            });
+            const testBtn = () => {data3.value = 100}
+            const data4 = reactive({x : 0 , result : 0});
+            watch(()=> data4.x, (c, o) => {
+                console.log(`${c} ${c}`);
+                data4.result++;
+            });
+            return {data,  result, clickBtn, data2, clickBtn2, data3, result3, testBtn, data4}
 
             // return {
             //     x : ref(10),
